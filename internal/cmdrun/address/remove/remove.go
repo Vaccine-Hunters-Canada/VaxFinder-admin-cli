c5e0ca251b18a875747996b14bd94972e6e8ada5
Eric St-Georges <gh@thevedge.net>
2021-05-18 16:18:13 -0400

    Added commands for address management

---
+++ internal/cmdrun/address/remove/remove.go
@@ -0,0 +1,60 @@
+package remove
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"strconv"
+	"vf-admin/internal/utils"
+
+	"github.com/fatih/color"
+	"github.com/spf13/cobra"
+)
+
+// CmdRunE is what's executed when running `vf-admin address remove <id>`
+func CmdRunE(cmd *cobra.Command, args []string) error {
+	id, aErr := strconv.Atoi(args[0])
+	if aErr != nil {
+		return errors.New("expecting id as integer")
+	}
+
+	// Retrieve the authentication key from configuration file
+	key, kErr := utils.GetKeyFromProfile(cmd)
+	if kErr != nil {
+		color.Red(kErr.Error())
+		return nil
+	}
+
+	// Create the API client using the authentication key for requests
+	client, cErr := utils.GetAPIClientFromKey(key)
+	if cErr != nil {
+		color.Red(cErr.Error())
+		return nil
+	}
+
+	// Create spinner
+	spinner, sErr := utils.GetDefaultSpinnerForHTTPOp("remove", "removed", fmt.Sprint("address ", id))
+	if sErr != nil {
+		color.Red(sErr.Error())
+		return nil
+	}
+	_ = spinner.Start()
+
+	res, rErr := client.DeleteAddressByIdApiV1AddressesAddressIdDeleteWithResponse(context.Background(), id)
+
+	if rErr != nil {
+		spinner.StopFailMessage(rErr.Error())
+		_ = spinner.StopFail()
+		return nil
+	}
+
+	if res.StatusCode() != 204 {
+		spinner.StopFailMessage(res.Status() + ": " + string(res.Body))
+		_ = spinner.StopFail()
+		return nil
+	}
+
+	_ = spinner.Stop()
+
+	return nil
+}
