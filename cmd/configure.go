ecc0b585e670382447ff53aec504c6d8b464f7d9
Alvin Tang <alvin.tang@mail.utoronto.ca>
2021-06-09 17:20:55 -0400

    refactor: Moved version and configure code away from cmdrun.

--- cmd/configure.go
+++ cmd/configure.go
@@ -1,9 +1,13 @@
 package cmd
 
 import (
+	"errors"
 	"github.com/MakeNowJust/heredoc"
+	"github.com/fatih/color"
+	"github.com/google/uuid"
 	"github.com/spf13/cobra"
-	"vf-admin/internal/cmdrun/configure"
+	"github.com/spf13/viper"
+	"time"
 )
 
 // Command: `vf-admin configure`
@@ -23,7 +27,28 @@ var configureCmd = &cobra.Command{
 			# set up authentication key for a custom profile
 			$ vf-admin configure --profile alvin --key 7260841f-b47a-4b5c-9830-585af07c4405
 	`),
-	RunE: configure.CmdRunE,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		profile, _ := cmd.Flags().GetString("profile")
+		key, _ := cmd.Flags().GetString("key")
+
+		// Return error if the key is not a UUID
+		if _, keyErr := uuid.Parse(key); keyErr != nil {
+			return errors.New("the key provided is not valid")
+		}
+
+		// TODO: Make REST call to verify key and get role type
+
+		// Update the configuration file for the specific profile
+		viper.Set(profile+".key", key)
+		viper.Set(profile+".role", 0)
+		viper.Set(profile+".updated", time.Now())
+		if writeErr := viper.WriteConfig(); writeErr != nil {
+			return writeErr
+		}
+
+		color.Green("Successfully updated configuration for the `%s` profile.\n", profile)
+		return nil
+	},
 }
 
 func init() {
--- cmd/version.go
+++ cmd/version.go
@@ -1,14 +1,27 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/spf13/cobra"
-	"vf-admin/internal/cmdrun/version"
+	"vf-admin/internal/utils"
 )
+
+var repo = "https://github.com/Vaccine-Hunters-Canada/VaxFinder-admin-cli"
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Returns the current version of the CLI",
-	Run:   version.CmdRun,
+	Run: func(cmd *cobra.Command, args []string) {
+		version, tag, date := utils.GetBuildInfo()
+
+		if tag == "unknown" && date == "unknown" {
+			fmt.Println("vf-admin/" + version)
+			fmt.Println(repo)
+		} else {
+			fmt.Println("vf-admin/v" + version + " (" + date + ")")
+			fmt.Println("https://github.com/Vaccine-Hunters-Canada/VaxFinder-admin-cli/releases/tag/" + tag)
+		}
+	},
 }
 
 func init() {
